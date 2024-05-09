b5d852052364bc73d1cc43d85310510a27ad3760
knight <knight@Ubuntu.myguest.virtualbox.org>
2024-05-09 10:40:16 -0300

    Delete tweet

--- src/main/java/com/patrick/springsecurity/controller/TweetController.java
+++ src/main/java/com/patrick/springsecurity/controller/TweetController.java
@@ -4,13 +4,13 @@ import com.patrick.springsecurity.controller.dto.CreateTweetDto;
 import com.patrick.springsecurity.entities.Tweet;
 import com.patrick.springsecurity.repository.TweetRepository;
 import com.patrick.springsecurity.repository.UserRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -32,6 +32,21 @@ public class TweetController {
         tweet.setContent(dto.content());
 
         tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId, JwtAuthenticationToken token){
+
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if(tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))){
+            tweetRepository.deleteById(tweet.getTweetId());
+        }else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
 
         return ResponseEntity.ok().build();
     }
